8e73d90f96395e52ccfa9de6284f5f5c1e8e545a
Andrey Grytsenko <andreyapg@gmail.com>
2026-06-11 12:14:10 +0300

    Implement `Person` class and `create_person_list` function to handle relationships

--- app/main.py
+++ app/main.py
@@ -1,8 +1,26 @@
 class Person:
-    # write your code here
-    pass
+
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        self.people[name] = self
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    person_instances = []
+
+    for person_dict in people:
+        person_instances.append(
+            Person(person_dict["name"], person_dict["age"])
+        )
+
+    for person_dict in people:
+        person_instance = Person.people[person_dict["name"]]
+        if "wife" in person_dict and person_dict["wife"] is not None:
+            person_instance.wife = Person.people[person_dict["wife"]]
+        if "husband" in person_dict and person_dict["husband"] is not None:
+            person_instance.husband = Person.people[person_dict["husband"]]
+
+    return person_instances
